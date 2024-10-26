bf3e92664cd399a86ca35c5beb6eda84c2eb38f2
vasileiosvyzas <vizasb@gmail.com>
2024-10-26 11:15:20 +0100

    New folder for the api with one of the endpoints

---
+++ app/api/endpoints/emissions_endpoint.py
@@ -0,0 +1,276 @@
+import json
+import boto3
+import time
+import os
+import math
+import random, string
+from datetime import datetime
+
+# Initialize the Athena client
+athena_client = boto3.client("athena")
+
+DATABASE = os.environ["DATABASE"]
+TABLE = os.environ["TABLE"]
+OUTPUT_LOCATION = os.environ["OUTPUT_LOCATION"]
+API_URL = os.environ['API_URL']
+
+def random_string(length):
+    return "".join(
+        random.choice(string.ascii_lowercase + string.digits) for _ in range(length)
+    )
+
+
+def execute_athena_query(query):
+    response = athena_client.start_query_execution(
+        QueryString=query,
+        QueryExecutionContext={"Database": DATABASE},
+        ResultConfiguration={"OutputLocation": OUTPUT_LOCATION},
+    )
+
+    query_execution_id = response["QueryExecutionId"]
+
+    # Wait for the query to complete (simplified for brevity)
+    while True:
+        query_status = athena_client.get_query_execution(
+            QueryExecutionId=query_execution_id
+        )
+        status = query_status["QueryExecution"]["Status"]["State"]
+        if status in ["SUCCEEDED", "FAILED", "CANCELLED"]:
+            break
+
+    if status == "SUCCEEDED":
+        results = athena_client.get_query_results(QueryExecutionId=query_execution_id)
+        columns = [
+            col["Label"]
+            for col in results["ResultSet"]["ResultSetMetadata"]["ColumnInfo"]
+        ]
+        rows = results["ResultSet"]["Rows"][1:]  # Skip the header row
+        return [
+            dict(zip(columns, [field.get("VarCharValue", "") for field in row["Data"]]))
+            for row in rows
+        ]
+    else:
+        raise Exception(f"Query failed with status: {status}")
+
+
+joined_table = f"""
+    WITH latest_versions AS (
+        SELECT CAST(year AS INTEGER) AS year, MAX(CAST(version AS INTEGER)) AS latest_version
+        FROM "{DATABASE}"."{TABLE}"
+        GROUP BY CAST(year AS INTEGER)
+    ),
+
+    latest_data AS (
+        SELECT *
+        FROM "{DATABASE}"."{TABLE}" se
+        JOIN latest_versions lv
+        ON CAST(se.year AS INT) = lv.year
+        AND CAST(se.version AS INT) = lv.latest_version
+    )
+"""
+
+base_query = """
+    SELECT imo_number, name, ship_type, reporting_period, total_co2_emissions, 
+        co2_emissions_from_all_voyages_between_ports_under_a_ms_jurisdiction, 
+        co2_emissions_from_all_voyages_which_departed_from_ports_under_a_ms_jurisdiction, 
+        co2_emissions_from_all_voyages_to_ports_under_a_ms_jurisdiction, 
+        co2_emissions_which_occurred_within_ports_under_a_ms_jurisdiction_at_berth, 
+        co2_emissions_assigned_to_passenger_transport, 
+        co2_emissions_assigned_to_freight_transport, co2_emissions_assigned_to_on_laden
+    FROM latest_data
+"""
+
+def get_total_results():
+    statement = f"""
+        SELECT COUNT(*) AS total_results
+        FROM latest_data
+    """
+    
+    query = joined_table + statement
+
+    total_results_value = execute_athena_query(query=query)
+    return total_results_value
+
+def emissions_data_without_conditions(page, limit):
+    offset = (page - 1) * limit
+    
+    pagination_query = f"""
+        ORDER BY imo_number, reporting_period DESC
+        OFFSET {offset}
+        LIMIT {limit}
+    """
+    
+    query = joined_table + base_query + pagination_query
+    data_response = execute_athena_query(query=query)
+    print(data_response)
+
+    print(get_total_results())
+    total_results = int(get_total_results()[0]['total_results'])
+
+    print(total_results)
+
+    total_pages = math.ceil(total_results / page)
+
+    print(total_results)
+    print(total_pages)
+    
+    if page < total_pages:
+        next_page_url = f"{API_URL}/emissions?page={page + 1}&limit=10"
+    else:
+        next_page_url = 'null'
+        
+    if page > 1:
+        prev_page_url = f"{API_URL}/emissions?page={page - 1}&limit=10"
+    else:
+        prev_page_url = 'null'
+    
+    return {
+        'metadata': {
+            "total_results": total_results,
+            "page": page,
+            "per_page": limit,
+            "total_pages": total_pages,
+            "next_page_url": next_page_url,
+            "prev_page_url": prev_page_url
+        },
+        'results': data_response
+    }
+    
+def emissions_per_ship_id(ship_id):
+    condition = f"""
+        WHERE imo_number = {ship_id}
+        ORDER BY reporting_period
+    """
+    query = joined_table + base_query + condition
+    response = execute_athena_query(query=query)
+    
+    statement = f"""
+        SELECT COUNT(*) AS total_results
+        FROM latest_data
+        WHERE imo_number = {ship_id}
+    """
+    
+    query = joined_table + statement
+
+    total_results_value = execute_athena_query(query=query)
+    total_results = int(total_results_value[0]['total_results'])
+    
+    return {
+        'metadata': {
+            "total_results": total_results,
+        },
+        'results': response
+    }
+
+def emissions_per_ship_type_and_year(ship_type, year, page, limit):
+    offset = (page - 1) * limit
+    
+    condition_query = f"""
+        WHERE WHERE ship_type = {ship_type} AND reporting_period={year};
+        ORDER BY imo_number, reporting_period DESC
+        OFFSET {offset}
+        LIMIT {limit}
+    """
+    
+    query = joined_table + base_query + condition_query
+    data_response = execute_athena_query(query=query)
+    print(data_response)
+
+    statement = f"""
+        SELECT COUNT(*) AS total_results
+        FROM latest_data
+        WHERE WHERE ship_type = {ship_type} AND reporting_period={year};
+    """
+    
+    query = joined_table + statement
+
+    total_results_value = execute_athena_query(query=query)
+    total_results = int(total_results_value[0]['total_results'])
+
+    total_pages = math.ceil(total_results / page)
+
+    print(total_results)
+    print(total_pages)
+    
+    if page < total_pages:
+        next_page_url = f"{API_URL}/emissions?ship_type={ship_type}&page={page + 1}&limit=10"
+    else:
+        next_page_url = 'null'
+        
+    if page > 1:
+        prev_page_url = f"{API_URL}/emissions?ship_type={ship_type}&page={page - 1}&limit=10"
+    else:
+        prev_page_url = 'null'
+    
+    return {
+        'metadata': {
+            "total_results": total_results,
+            "page": page,
+            "per_page": limit,
+            "total_pages": total_pages,
+            "next_page_url": next_page_url,
+            "prev_page_url": prev_page_url
+        },
+        'results': data_response
+    }
+
+def lambda_handler(event, context):
+    print(event)
+    # ship_type = event["queryStringParameters"]["ship_type"]
+    
+    # year = event["queryStringParameters"]["year"]
+    
+    # offset = (page - 1) * limit
+
+    if event["queryStringParameters"] == None:
+        # default values for page and limit if they are missing
+        page = 1
+        limit = 10
+        
+        response = emissions_data_without_conditions(page=page, limit=limit)
+    
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(response),
+        }
+        
+    if 'page' in event["queryStringParameters"]:
+        page = int(event["queryStringParameters"]["page"])
+        limit = int(event["queryStringParameters"]["limit"])
+        
+        response = emissions_data_without_conditions(page=page, limit=limit)
+    
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(response),
+        }
+    
+    
+    if 'ship_id' in event['queryStringParameters']:
+        ship_id = event["queryStringParameters"]["ship_id"]
+        
+        response = emissions_per_ship_id(ship_id=ship_id)
+        
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(response),
+        }    
+    
+    if 'page' in event["queryStringParameters"] and 'ship_type' in event["queryStringParameters"] and 'limit' in event["queryStringParameters"] and 'year' in event["queryStringParameters"]:
+        ship_type = event["queryStringParameters"]["ship_type"]
+        page = int(event["queryStringParameters"]["page"])
+        limit = int(event["queryStringParameters"]["limit"])
+        year = int(event["queryStringParameters"]["year"])
+
+        response = emissions_per_ship_type_and_year(ship_type=ship_type, year=year, page=page, limit=limit)
+
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(response),
+        }
+
+    
