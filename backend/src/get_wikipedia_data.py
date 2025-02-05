f277ac694ac0e3267e077dcd9f2c764cf8a56b67
vasileiosvyzas <vizasb@gmail.com>
2025-02-05 09:17:22 +0000

    fixed conflict

--- backend/src/get_wikipedia_data.py
+++ backend/src/get_wikipedia_data.py
@@ -1,12 +1,6 @@
 import wikipedia
 import json
 import time
-import os
-
-import pandas as pd
-import numpy as np
-import boto3
-import awswrangler as wr
 
 from bs4 import BeautifulSoup
 from difflib import SequenceMatcher
@@ -14,40 +8,6 @@ from dotenv import load_dotenv
 from src.utils.data import fetch_ship_ids
 
 load_dotenv()
-
-# def get_unique_ship_id_list():
-#     DATABASE = os.environ['DATABASE']
-#     TABLE = os.environ['TABLE']
-#     OUTPUT_LOCATION = os.environ['QUERY_LOCATION']
-    
-#     my_session = boto3.session.Session(
-#         region_name=os.environ['REGION'], 
-#         aws_access_key_id=os.environ['ACCESS_KEY'], 
-#         aws_secret_access_key=os.environ['SECRET']
-#     )
-    
-#     query = f"""
-#         WITH latest_versions AS (
-#             SELECT CAST(year AS INTEGER) AS year, MAX(CAST(version AS INTEGER)) AS latest_version
-#             FROM "{DATABASE}"."{TABLE}"
-#             GROUP BY CAST(year AS INTEGER)
-#         ),
-
-#         latest_data AS (
-#             SELECT *
-#             FROM "{DATABASE}"."{TABLE}" se
-#             JOIN latest_versions lv
-#             ON CAST(se.year AS INT) = lv.year
-#             AND CAST(se.version AS INT) = lv.latest_version
-#         )
-        
-#         SELECT DISTINCT imo_number, name FROM latest_data;
-#     """
-    
-#     distinc_imo_numbers = wr.athena.read_sql_query(query, database=DATABASE, boto3_session=my_session)
-#     print(distinc_imo_numbers.shape)
-    
-#     return distinc_imo_numbers
 
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
